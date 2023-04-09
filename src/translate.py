5ce8a5d0a9de1514c8ae981b30d6e3c28f61737e
Abraham <abraham@arnold.home>
2023-04-09 20:04:06 +0300

    Trnaslated the first part in Finnish. Cleaned up the code.

--- src/translate.py
+++ src/translate.py
@@ -1,25 +1,28 @@
 import logging
 import os
+import sys
 import deepl
 import requests
-import sys
+
 
 LOGGER = logging.getLogger(__name__)
-ALLOWED_FILE_TYPES = {'txt', 'pdf'}
+ALLOWED_FILE_TYPES = {"txt", "pdf"}
 
 
 def get_char_size(content: str) -> int:
-    return len(content.encode('utf-8'))
+    return len(content.encode("utf-8"))
 
 
-def open_file(relative_path: str, mode: str = 'r') -> str:
+def open_file(relative_path: str, mode: str = "r") -> str:
     module_path = os.path.abspath(__file__)
     path = os.path.join(os.path.dirname(module_path), relative_path)
     with open(path, mode) as f:
         return f.read()
 
 
-def write_file(relative_path: str, content: str, target_lang: str, mode: str = 'w') -> str:
+def write_file(
+    relative_path: str, content: str, target_lang: str, mode: str = "w"
+) -> str:
     module_path = os.path.abspath(__file__)
     path = os.path.join(os.path.dirname(module_path), relative_path)
     with open(path, mode) as f:
@@ -28,70 +31,104 @@ def write_file(relative_path: str, content: str, target_lang: str, mode: str = '
 
 def _validate_file(file_suffix: str, content: str) -> None:
     if file_suffix not in ALLOWED_FILE_TYPES:
-        raise ValueError('File type not allowed. Only {} are allowed.'.format(ALLOWED_FILE_TYPES))
+        raise ValueError(
+            "File type not allowed. Only {} are allowed.".format(ALLOWED_FILE_TYPES)
+        )
     if not content:
-        raise ValueError('File is empty.')
+        raise ValueError("File is empty.")
     if get_char_size(content) > 5000000:
-        raise ValueError('File is too large. Max size is 5MB.')
+        raise ValueError("File is too large. Max size is 5MB.")
     return True
 
 
 def _split_content(content: str, chars_per_chunk: int = 5000) -> list[str]:
     """Splits text (linewise) into smaller chunks based on chars_per_chunk."""
     chunks = []
-    chunk = ''
+    chunk = ""
     for line in content.splitlines():
         if get_char_size(chunk) + get_char_size(line) > chars_per_chunk:
             chunks.append(chunk)
-            chunk = ''
-        chunk += line + '\n'
+            chunk = ""
+        chunk += line + "\n"
     if chunk:
-        chunks.append(chunk[:-1]) # The last separator must be omitted.
+        chunks.append(chunk[:-1])  # The last separator must be omitted.
     return chunks
 
 
-def translate_with_google(content: str, target_lang: str = 'en') -> str:
-    url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={}&dt=t&q={}'
+def translate_with_google(content: str, target_lang: str = "en") -> str:
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={}&dt=t&q={}"
     url = url.format(target_lang, content)
     response = requests.get(url)
     if response.status_code != 200:
-        print('Error: the translate API returned an unsuccessful status code {}'.format(response.status_code))
+        print(
+            "Error: the translate API returned an unsuccessful status code {}".format(
+                response.status_code
+            )
+        )
         sys.exit()
-    return ''.join(t[0] for t in response.json()[0])
+    return "".join(t[0] for t in response.json()[0])
 
 
 def _get_deepl_token() -> str:
     return os.getenv("DEEPL_TOKEN")
 
 
-def _translate_with_deepl(content: str, target_lang: str = 'EN') -> str:
+def translate_with_deepl(
+    content: str, target_lang: str = "EN", chars_per_chunk: int = 5000
+) -> str:
     """Translate the given text into the target language with deepl api.
     Use the access token to authenticate your request."""
+    chunks = _split_content(content, chars_per_chunk=chars_per_chunk)
     translator = deepl.Translator(_get_deepl_token())
+    results: list[str] = [
+        _translate_with_deepl(translator, chunk, target_lang=target_lang)
+        for chunk in chunks
+    ]
+    return "".join(result for result in results)
+
+
+def _translate_with_deepl(
+    translator: deepl.Translator, content: str, target_lang: str = "EN"
+) -> str:
+    """Translate the given text into the target language with deepl api."""
+    # translator = deepl.Translator(_get_deepl_token())
     result = translator.translate_text(content, target_lang=target_lang)
     raw_text = result.text
-    text = raw_text.replace('SIVUNVAIHTO', 'PAGE BREAK')
-    return text
+    return raw_text.replace("SIVUNVAIHTO", "PAGE BREAK")
 
 
-def translate_file(file_path, target_lang='EN') -> None: 
+def translate_file(file_path, target_lang="EN", chars_per_chunk: int = 5000) -> None:
     content = open_file(file_path)
-    file_suffix = file_path.split('.')[-1]
+    file_suffix = file_path.split(".")[-1]
     _validate_file(file_suffix, content)
-    res = _translate_with_deepl(content, target_lang=target_lang)
-    saved_file_path = file_path.split('.' + file_suffix)[0] + '_' + target_lang.lower() + '.' + file_suffix
+    res = translate_with_deepl(
+        content, target_lang=target_lang, chars_per_chunk=chars_per_chunk
+    )
+    saved_file_path = (
+        file_path.split("." + file_suffix)[0]
+        + "_"
+        + target_lang.lower()
+        + "."
+        + file_suffix
+    )
     write_file(saved_file_path, res, target_lang=target_lang)
 
 
-if __name__ == '__main__':
-    first_part = '../data/the_future_chapters_1-20.txt'
-    second_part = '../data/the_future_chapters_21-37.txt'
-    third_part = '../data/the_future_chapters_38-46.txt'
+if __name__ == "__main__":
+    first_part = "../data/the_future_chapters_1-20.txt"
+    second_part = "../data/the_future_chapters_21-37.txt"
+    third_part = "../data/the_future_chapters_38-46.txt"
     content = open_file(first_part)
     print(get_char_size(content))
     content = open_file(second_part)
     print(get_char_size(content))
     content = open_file(third_part)
     print(get_char_size(content))
+    # Run at 9th April -23
+    # translate_file(first_part, 'FI', chars_per_chunk=4000)
 
-    #translate_file(first_part, 'FI')
+    # To be run at 9th May -23
+    # translate_file(second_part, 'FI', chars_per_chunk=4000)
+
+    # To be run at 9th June -23
+    # translate_file(third_part, 'FI', chars_per_chunk=4000)
--- tests/test_1000_translate.py
+++ tests/test_1000_translate.py
@@ -1,10 +1,29 @@
 import pytest
 from src import translate
 
-SMALL_TEXT = """“Oh come on, you had me until…” My mother waved her hand.
+SHORT_TEXT = """“Oh come on, you had me until…” My mother waved her hand.
 “Maybe, maybe, maybe… That’s just – acid conjecture, like the ‘we are in a simulation’ lunatics. Here are the facts: Joan is a plain woman in her fifties who’s going to find out that the wonderful world she is setting out to join is just a refugee ship of broken people with broken lives, and she doesn’t have the sexual power she had in her twenties, and she’s going to end up burying her regrets in cat litter. And all the money she thinks she’s going to get from Bill, well that’s just going to the lawyers and two houses and eighteen vacations. It’s pathetic, and I hate her with all my heart for sowing this stupidity in the minds of her friends. She screwed up, she got greedy, she overreached – and now she’s fallen out of the cruise ship, and we cannot circle back to find her.”
 My mother gnawed at her thumb. “She’s so sad, she needs someone…”
 ----- PAGE BREAK ----- 317"""
+
+MEDIUM_TEXT = """----- PAGE BREAK ----- 317
+“She had someone – she had Bill and a decent marriage. But someone put the worm in her ear, and now she’s trying to put the worm in your ear – don’t let her, don’t do it, it’s a hole with no bottom, you know it!”
+“So I’m supposed to just – abandon her, in her hour of need?”
+My father shrugged angrily. “She abandoned Bill. She’s got nothing to complain about.”
+They moved on, and their voices no longer slid round the semicircle of the stone statue’s outstretched hands to my cupped ears.
+I’d heard enough. Women had to be wrangled, their sympathies were their weakness.
+Or, as my father once told me, “Women will always find someone or something to mother – if they don’t have kids, it’s cats or immigrants.”
+I sometimes wished that the whole world was women, when I was running for office.
+Anyway, I grew sick of my own inner ramblings – I felt a sudden savage yearning for action, and instantly remembered my own eldest son, who I had grudgingly taken to a petting zoo when he was young, because – according to my wife – he was turning into a little brat in pre‐K.
+He was so goddamned mournful, that boy, almost from the beginning – he moped and sighed, and was congenitally ungrateful, which drove me quite mad – I fantasized that my wife had had an affair with some man‐bun soy‐drip barista at the local coffee shop.
+Jake just talked and talked – he once complained that he was dying of boredom.
+“You can’t die of boredom!” I snapped.
+“How do you know?”
+“Because I’ve spent months listening to you go on and on about that Robocraft game!”
+I had to savagely cut him off when he went on these – “rambletangents” I called them. Endless stories of his dreams and ideas and plans – I didn’t even bother to listen, just thought my own thoughts and went all Zen and into the future, like he was a trickling brook of vapid nonsense.
+My daughter was a talker too, but you could get a word in edgewise. I remember her describing her ideal restaurant once – in the woods, high in a treetop, birds on the leaves, rope swings and koi ponds – it was entertaining enough to interest me… I added in some robot waiters, and it was actually kind of fun…
+But my eldest son, the whiner who wouldn’t get a haircut until I held him down, I spend most of his childhood just – shutting him up. He was like an impossible leaky house – every hole you patched just bled drippy water somewhere else.
+----- PAGE BREAK ----- 318"""
 
 TEXT = """“Oh come on, you had me until…” My mother waved her hand.
 “Maybe, maybe, maybe… That’s just – acid conjecture, like the ‘we are in a simulation’ lunatics. Here are the facts: Joan is a plain woman in her fifties who’s going to find out that the wonderful world she is setting out to join is just a refugee ship of broken people with broken lives, and she doesn’t have the sexual power she had in her twenties, and she’s going to end up burying her regrets in cat litter. And all the money she thinks she’s going to get from Bill, well that’s just going to the lawyers and two houses and eighteen vacations. It’s pathetic, and I hate her with all my heart for sowing this stupidity in the minds of her friends. She screwed up, she got greedy, she overreached – and now she’s fallen out of the cruise ship, and we cannot circle back to find her.”
@@ -41,40 +60,56 @@ Jake was picking up tiny leaves and plants, trying to feed them to the ducks, wh
 “Cones are two dollars, three for five.” She pointed at a sign which said exactly the same thing – which irritated me even more.
 ----- PAGE BREAK ----- 319"""
 
+
 def test_open_file():
-    assert'“So I’m supposed to just – abandon her, in her hour of need?”' in  translate.open_file('../data/example.txt')
+    assert (
+        "“So I’m supposed to just – abandon her, in her hour of need?”"
+        in translate.open_file("../data/example.txt")
+    )
+
 
 def test_validate_file():
-    assert translate._validate_file('txt', 'Hello') == True
+    assert translate._validate_file("txt", "Hello") == True
     with pytest.raises(ValueError):
-        translate._validate_file('humbug', 'Hello')
+        translate._validate_file("humbug", "Hello")
+
 
 def test_translate_with_google():
-    assert translate.translate_with_google('Hello', 'fi') == 'Hei'
+    assert translate.translate_with_google("Hello", "fi") == "Hei"
+
 
 def test_translate_with_deepl():
-    assert translate._translate_with_deepl('Hello', 'fi') == 'Hei'
+    assert translate._translate_with_deepl("Hello", "fi") == "Hei"
+
 
 def test_split_content():
-    chunks = translate._split_content("Hello world! \n", '', chars_per_chunk=5000)
-    print(len(chunks))
-    total_size = len(TEXT.encode('utf-8'))
+    chunks = translate._split_content("Hello world! \n", chars_per_chunk=5000)
+    total_size = len(TEXT.encode("utf-8"))
     assert 10000 > total_size > 5000
-    chunks = translate._split_content(TEXT, '', chars_per_chunk=5000)
+    chunks = translate._split_content(TEXT, chars_per_chunk=5000)
     assert len(chunks) == 2
-    assert '----- PAGE BREAK ----- 319' in chunks[-1]
+    assert "----- PAGE BREAK ----- 319" in chunks[-1]
 
-    whole_text = ''.join(chunks)
-    '''print(TEXT)
-    print(whole_text)
-    assert whole_text == TEXT'''
+    whole_text = "".join(chunks)
+    total_size = len(SHORT_TEXT.encode("utf-8"))
+    assert 1000 > total_size > 900
 
-    chunks = translate._split_content(SMALL_TEXT, '', chars_per_chunk=100)
-    assert len(chunks) == 3
-    whole_text = ''.join(chunks)
-    print(SMALL_TEXT)
-    print(len(SMALL_TEXT))
-    print(len(whole_text))
-    assert SMALL_TEXT == whole_text
+    chunks = translate._split_content(SHORT_TEXT, chars_per_chunk=100)
+    assert len(chunks) == 3 == len(SHORT_TEXT) / 100
+    whole_text = "".join(chunks)
+    assert SHORT_TEXT == whole_text
 
-    
+
+def test_translate_file(monkeypatch):
+    def mock_translate_with_deepl(
+        translator, content: str, target_lang: str = "EN"
+    ) -> str:
+        return content.replace("SIVUNVAIHTO", "PAGE BREAK")
+
+    monkeypatch.setattr(translate, "_translate_with_deepl", mock_translate_with_deepl)
+    translate.translate_file("../data/example.txt", "FI", chars_per_chunk=4000)
+    with open("data/example_fi.txt", "r") as f:
+        read_content = f.read()
+
+    assert "She had someone – " in read_content
+    assert MEDIUM_TEXT == read_content
